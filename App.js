35f5d2bc5751f062dc7f2915fe75349964d57114
todmy <tolok.d@gmail.com>
2021-07-29 13:15:07 +0300

    added example

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Can from './Can'
 
 export default function App() {
+  const post = { read: true }
   return (
-    <View style={styles.container}>
+    <View style={styles.container}>   
       <Text>Open up App.js to start working on your app!</Text>
+      <Can I="read" a="Line">
+        <Text>I can read it!</Text>
+      </Can>
+      <Can I="read" a="Text">
+        <Text>I can't read it!</Text>
+      </Can>
       <StatusBar style="auto" />
     </View>
   );
